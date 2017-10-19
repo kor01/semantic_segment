c11a5e7085d1c9624520b90790ecf8734b9d036c
chenpu01 <chenpu01@baidu.com>
2017-10-19 20:55:01 +0800

    add fcn

---
+++ segment_net/fcn.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+
+tf.flags.DEFINE_integer('encode_filters', 32, 'number pf filters')
+tf.flags.DEFINE_integer('decode_filters', 64, 'number of filters in decoder phase')
+tf.flags.DEFINE_integer('num_encoders', 4, 'number of encoding layer')
+tf.flags.DEFINE_integer('scale', 2, 'scale in pyramid')
+
+FLAGS = tf.flags.FLAGS
+
+
+def encoder(img, scale):
+
+  with tf.variable_scope('encoder'):
+
+    def conv_and_pool(feature):
+      conv = tf.layers.separable_conv2d(
+        feature, FLAGS.filters, 3, 1,
+        padding='SAME', activation=tf.nn.relu)
+
+      pool = tf.layers.max_pooling2d(
+        conv, scale, scale, padding='SAME')
+      return pool
+
+    pools = [img]
+
+    for i in range(FLAGS.num_encoders):
+      with tf.variable_scope('encoder-%d' % i):
+        pools.append(conv_and_pool(pools[-1]))
+
+    pools.reverse()
+    return pools
+
+
+def decoder(pools, scale, num_classes):
+
+  last_pool = pools[0]
+  filters = FLAGS.decode_filters
+
+  last_pool = tf.layers.conv2d(
+    last_pool, filters=filters,
+    kernel_size=1, activation=tf.nn.relu)
+
+  for pool in pools[1:]:
+    pool = tf.layers.conv2d_transpose(
+      pool, filters, scale, scale, activation=tf.nn.relu)
+    pool = tf.concat([pool, last_pool], axis=-1)
+    last_pool = tf.layers.separable_conv2d(
+      pool, filters=filters, kernel_size=3,
+      padding='SAME', activation=tf.nn.relu)
+
+  logits = tf.layers.conv2d(
+    last_pool, filters=num_classes, kernel_size=1)
+
+  return logits
+
+
+def apply_fcn(images, num_classes, is_train):
+
+  with tf.variable_scope('fcn'):
+    pools = encoder(images, FLAGS.scale)
+    logits = decoder(pools, FLAGS.scale, num_classes)
+  return logits
