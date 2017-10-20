67da51604c094c92a424b93422a10c33341b995a
chenpu01 <chenpu01@baidu.com>
2017-10-20 09:25:55 +0800

    remove the unused baseline

--- segment_net/model.py
+++ segment_net/model.py
@@ -1,10 +1,8 @@
-from segment_net.baseline import fcn_model
 from segment_net.fcn_baseline import fcn_baseline
 from segment_net.fcn import apply_fcn
 
 
-NETWORKS = {'fcn_model': fcn_model,
-            'fcn_baseline': fcn_baseline, 'fcn': apply_fcn}
+NETWORKS = {'fcn_baseline': fcn_baseline, 'fcn': apply_fcn}
 
 
 def get_network(name):
